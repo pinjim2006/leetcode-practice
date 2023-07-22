955ef296a23467dcb5561798a2f54bfe5040fb30
PinJimWu <pinjim2006@gmail.com>
2023-07-22 11:04:53 +0800

    Add/Update add-digits (c)

---
+++ 0258_add-digits/add-digits.c
@@ -0,0 +1,11 @@
+int addDigits(int num){
+    while(num>9){
+        int tmp=0;
+        while(num>0){
+            tmp+=num%10;
+            num/=10;
+        }
+        num=tmp;
+    }
+    return num;
+}
