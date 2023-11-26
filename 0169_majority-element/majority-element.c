8363595ef2ae870a7f805010330000b1abefaa4f
PinJimWu <pinjim2006@gmail.com>
2023-11-26 15:02:10 +0800

    Add/Update majority-element (c)

---
+++ 0169_majority-element/majority-element.c
@@ -0,0 +1,14 @@
+int majorityElement(int* nums, int numsSize) {
+    int candidate=nums[0],count=1;
+    for(int i=1;i<numsSize;i++){
+        if(count==0){
+            candidate=nums[i];
+            count=1;
+        }else if(candidate==nums[i]){
+            count++;
+        }else{
+            count--;
+        }
+    }
+    return candidate;
+}
