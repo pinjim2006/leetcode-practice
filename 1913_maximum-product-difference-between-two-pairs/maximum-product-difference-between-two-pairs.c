9b69cfe57c5478f30d00548c23e37b4863a09c8d
PinJimWu <pinjim2006@gmail.com>
2023-12-18 18:19:56 +0800

    Add/Update 1913_maximum-product-difference-between-two-pairs (c)

---
+++ 1913_maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.c
@@ -0,0 +1,8 @@
+int cmp(const void *a,const void *b){
+    return *(int*)a-*(int*)b;
+}
+
+int maxProductDifference(int* nums, int numsSize){
+    qsort(nums,numsSize,sizeof(int),cmp);
+    return nums[numsSize-1]*nums[numsSize-2]-nums[0]*nums[1];
+}
