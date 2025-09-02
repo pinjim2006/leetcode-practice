b21c284f712e02937d598a78958209dbd42b9d91
PinJimWu <pinjim2006@gmail.com>
2025-09-03 00:47:50 +0800

    Add/Update hamming-distance (java)

--- 0461_hamming-distance/hamming-distance.java
+++ 0461_hamming-distance/hamming-distance.java
@@ -1,12 +1,5 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int result = x ^ y;
-        int ans = 0;
-
-        while(result > 0){
-            ans += result & 1;
-            result >>= 1;
-        }
-        return ans;
+        return Integer.bitCount(x ^ y);
     }
 }
