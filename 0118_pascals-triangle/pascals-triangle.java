c7d9a7e6dbdd295a6596b9cf97dcd58e0b79fe61
PinJimWu <pinjim2006@gmail.com>
2025-09-05 23:56:07 +0800

    Add/Update 118_pascals-triangle (java)

---
+++ 0118_pascals-triangle/pascals-triangle.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        result.get(0).add(1);
+        for(int i = 1; i < numRows; i++){
+            result.add(new ArrayList<>());
+            result.get(i).add(1);
+            for(int j = 1; j < i; j++){
+                result.get(i).add( result.get(i-1).get(j-1) + result.get(i-1).get(j) );
+            }
+            result.get(i).add(1);
+        }
+        return result;
+    }
+}
