74e485c0228c5ac32fa97b957cfda26226d20381
PinJimWu <pinjim2006@gmail.com>
2025-09-08 19:49:02 +0800

    Add/Update pascals-triangle-ii (java)

---
+++ 0119_pascals-triangle-ii/pascals-triangle-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0; i <= rowIndex; i++){
+            result.add(1);
+        }
+        for(int i = 2; i <= rowIndex; i++){
+            for(int j = i - 1; j > 0; j--){
+                result.set(j, result.get(j) + result.get(j-1));
+            }
+        }
+        return result;
+    }
+}
