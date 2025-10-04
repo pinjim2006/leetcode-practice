672d0d306adff57c0c0f4efd4b0c0b348f50ffd3
PinJimWu <pinjim2006@gmail.com>
2025-10-05 01:31:06 +0800

    Add/Update container-with-most-water (java)

---
+++ 0011_container-with-most-water/container-with-most-water.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+        while(left < right){
+            int h = Math.min(height[left], height[right]);
+            int w = right - left;
+            maxArea = maxArea > h * w ? maxArea : h * w;
+            if(height[left] > height[right]){
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return maxArea;
+    }
+}
