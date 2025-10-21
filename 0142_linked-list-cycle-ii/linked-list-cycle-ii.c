27ee2abb786c29449326daa33b06c8d7bc9ac5ea
PinJimWu <pinjim2006@gmail.com>
2025-10-21 20:49:12 +0800

    Add/Update linked-list-cycle-ii (c)

---
+++ 0142_linked-list-cycle-ii/linked-list-cycle-ii.c
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *detectCycle(struct ListNode *head) {
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast) break;
+    }
+    if(!fast || !fast->next) return NULL;
+    slow = head;
+    while(slow != fast){
+        slow = slow->next;
+        fast = fast->next;
+    }
+    return slow;
+}
