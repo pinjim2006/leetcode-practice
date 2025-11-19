2c9d7357659db5214c0cb62fb461a32604218486
PinJimWu <pinjim2006@gmail.com>
2025-11-19 16:25:45 +0800

    Add/Update keep-multiplying-found-values-by-two (c)

---
+++ 2154_keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.c
@@ -0,0 +1,8 @@
+int findFinalValue(int* nums, int numsSize, int original) {
+    for(int i = 0; i < numsSize; i++){
+        if(nums[i] == original){
+            return findFinalValue(nums, numsSize, original*2);
+        }
+    }
+    return original;
+}
