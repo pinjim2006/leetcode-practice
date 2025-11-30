8249336d3ee6db0d74528d176b4292175bcb328c
PinJimWu <pinjim2006@gmail.com>
2025-11-30 23:58:01 +0800

    Add/Update symmetric-tree (c)

--- 0101_symmetric-tree/symmetric-tree.c
+++ 0101_symmetric-tree/symmetric-tree.c
@@ -6,13 +6,13 @@
  *     struct TreeNode *right;
  * };
  */
-bool sameMirrorTree(struct TreeNode* p, struct TreeNode* q){
-    if(p == NULL && q == NULL) return true;
-    if(p == NULL || q == NULL) return false;
+bool isSymmetricTree(struct TreeNode* p, struct TreeNode* q){
+    if((p == NULL) ^ (q == NULL))return false;
+    if(p == NULL) return true;
     if(p->val != q->val) return false;
-    return sameMirrorTree(p->left, q->right) && sameMirrorTree(p->right, q->left);
+    return isSymmetricTree(p->left, q->right) && isSymmetricTree(p->right, q->left);
 }
 
 bool isSymmetric(struct TreeNode* root) {
-    return sameMirrorTree(root->left, root->right);
+    return isSymmetricTree(root->left, root->right);
 }
