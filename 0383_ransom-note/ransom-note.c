298a7e9ec665041f7685f00a7230eff95e153410
PinJimWu <pinjim2006@gmail.com>
2025-12-22 22:58:55 +0800

    Add/Update ransom-note (c)

--- 0383_ransom-note/ransom-note.c
+++ 0383_ransom-note/ransom-note.c
@@ -1,11 +1,15 @@
 bool canConstruct(char* ransomNote, char* magazine) {
     int count[26] = {0};
-    for(int i = 0; i < strlen(magazine); i++){
+    int mlen = strlen(magazine);
+    for(int i = 0; i < mlen; i++){
         count[magazine[i] - 'a']++;
     }
-    for(int i = 0; i < strlen(ransomNote); i++){
+
+    int rlen = strlen(ransomNote);
+    for(int i = 0; i < rlen; i++){
         if(count[ransomNote[i] - 'a'] <= 0) return false;
         else count[ransomNote[i] - 'a']--;
     }
+    
     return true;
 }
