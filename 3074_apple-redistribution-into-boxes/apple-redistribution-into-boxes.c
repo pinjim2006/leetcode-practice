42349079d4e27887024c68bd18f515b63799e15c
PinJimWu <pinjim2006@gmail.com>
2025-12-25 01:46:34 +0800

    Add/Update apple-redistribution-into-boxes (c)

---
+++ 3074_apple-redistribution-into-boxes/apple-redistribution-into-boxes.c
@@ -0,0 +1,15 @@
+int cmp (const void * a, const void * b){
+   return ( *(int*)b - *(int*)a );
+}
+
+int minimumBoxes(int* apple, int appleSize, int* capacity, int capacitySize) {
+    qsort(capacity, capacitySize, sizeof(int), cmp);
+    int total = 0;
+    for(int i = 0; i < appleSize; i++) total += apple[i];
+    int ans = 0;
+    while(total > 0){
+        total -= capacity[ans];
+        ans++;
+    }
+    return ans;
+}
