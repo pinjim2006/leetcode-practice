ba059c7c7ec697beca57880f4c70a8de881023b1
PinJimWu <pinjim2006@gmail.com>
2025-12-27 17:25:36 +0800

    Add/Update 3289_the-two-sneaky-numbers-of-digitville (c)

--- 3289_the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.c
+++ 3289_the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.c
@@ -6,13 +6,16 @@ int* getSneakyNumbers(int* nums, int numsSize, int* returnSize) {
     *returnSize = 2;
     int n = numsSize - 2;
     int check[n];
+    int count = 0;
     memset(check, 0, sizeof(check));
     for(int i = 0; i < numsSize; i++){
         check[nums[i]]++;
+        if(check[nums[i]] == 2) result[count++] = nums[i];
     }
-    int count = 0;
-    for(int i = 0; i < n; i++){
-        if(check[i] == 2) result[count++] = i;
+    if(result[0] > result[1]){
+        int tmp = result[0];
+        result[0] = result[1];
+        result[1] = tmp;
     }
     return result;
 }
