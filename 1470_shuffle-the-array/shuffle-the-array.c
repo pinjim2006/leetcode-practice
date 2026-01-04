7c0740df47eef90eb57a946353caf73c7dfd2488
PinJimWu <pinjim2006@gmail.com>
2026-01-04 08:24:30 +0800

    Add/Update shuffle-the-array (c)

---
+++ 1470_shuffle-the-array/shuffle-the-array.c
@@ -0,0 +1,14 @@
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* shuffle(int* nums, int numsSize, int n, int* returnSize){
+    int* result = (int*)malloc(sizeof(int) * numsSize);
+    *returnSize = numsSize;
+    for(int i = 0; i < n; i++){
+        result[i*2] = nums[i];
+        result[i*2+1] = nums[i + n];
+    }
+    return result;
+}
