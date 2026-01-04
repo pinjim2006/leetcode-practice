e40c4dac5ecb8fe29979a0c12905bb946a8335c8
PinJimWu <pinjim2006@gmail.com>
2026-01-04 08:21:13 +0800

    Add/Update concatenation-of-array (c)

--- 1929_concatenation-of-array/concatenation-of-array.c
+++ 1929_concatenation-of-array/concatenation-of-array.c
@@ -2,11 +2,11 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* getConcatenation(int* nums, int numsSize, int* returnSize) {
-    int* result=(int*)malloc((numsSize*2)*sizeof(int));
-    *returnSize=numsSize*2;
-    for(int i=0;i<numsSize;i++){
-        result[i]=nums[i];
-        result[i+numsSize]=nums[i];
+    int* result = (int*)malloc(sizeof(int) * numsSize * 2);
+    *returnSize = numsSize * 2;
+    for(int i = 0; i < numsSize; i++){
+        result[i] = nums[i];
+        result[i + numsSize] = nums[i];
     }
     return result;
 }
