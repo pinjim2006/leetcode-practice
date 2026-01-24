7a1e9211849dc4d542cc6c31a47211e0e6312c00
PinJimWu <pinjim2006@gmail.com>
2026-01-24 20:50:57 +0800

    Add/Update minimize-maximum-pair-sum-in-array (c)

--- 1877_minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.c
+++ 1877_minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.c
@@ -1,12 +1,12 @@
-int cmp(const void *a, const void *b){
-    return *(int*)a-*(int*)b;
+int cmp(const void* a, const void* b){
+    return *(int*)a - *(int*)b;
 }
 
 int minPairSum(int* nums, int numsSize){
-    int result=0;
-    qsort(nums,numsSize,sizeof(int),cmp);
-    for(int i=0;i<numsSize/2;i++){
-        result=(nums[i]+nums[numsSize-i-1])>result?(nums[i]+nums[numsSize-i-1]):result;
+    qsort(nums, numsSize, sizeof(int), cmp);
+    int maxSum = 0;
+    for(int i = 0; i < numsSize / 2; i++){
+        maxSum = maxSum > (nums[i] + nums[numsSize-i-1]) ? maxSum : (nums[i] + nums[numsSize-i-1]);
     }
-    return result;
+    return maxSum;
 }
