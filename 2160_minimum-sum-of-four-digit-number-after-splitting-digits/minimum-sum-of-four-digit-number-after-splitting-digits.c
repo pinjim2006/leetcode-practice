f556a8cdab65739dc14571ad45d3e1543ce2f6eb
PinJimWu <pinjim2006@gmail.com>
2026-01-26 20:34:29 +0800

    Add/Update minimum-sum-of-four-digit-number-after-splitting-digits (c)

---
+++ 2160_minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.c
@@ -0,0 +1,15 @@
+int cmp(const void* a, const void* b){
+    return *(int*)a - *(int*)b;
+}
+
+int minimumSum(int num) {
+    int nums[4];
+    for(int i = 0; i < 4; i++){
+        nums[i] = num % 10;
+        num /= 10;
+    }
+
+    qsort(nums, 4, sizeof(int), cmp);
+
+    return (nums[0] + nums[1]) * 10 + nums[2] + nums[3];
+}
