a0ea7c34f7341a444142e064bdbcf4da3b814fab
PinJimWu <pinjim2006@gmail.com>
2026-02-27 16:28:04 +0800

    Add/Update make-two-arrays-equal-by-reversing-subarrays (c)

---
+++ 1460_make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.c
@@ -0,0 +1,12 @@
+int cmp(const void* a, const void* b){
+    return *(int*)a - *(int*)b;
+}
+
+bool canBeEqual(int* target, int targetSize, int* arr, int arrSize) {
+    qsort(target, targetSize, sizeof(int), cmp);
+    qsort(arr, arrSize, sizeof(int), cmp);
+    for(int i = 0; i < targetSize; i++){
+        if(target[i] != arr[i]) return false;
+    }
+    return true;
+}
