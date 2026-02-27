260bdbd8fce86b86e1a60c196e6f4a8ff5589c54
PinJimWu <pinjim2006@gmail.com>
2026-02-28 01:39:20 +0800

    Add/Update number-of-arithmetic-triplets (c)

---
+++ 2367_number-of-arithmetic-triplets/number-of-arithmetic-triplets.c
@@ -0,0 +1,20 @@
+int arithmeticTriplets(int* nums, int numsSize, int diff) {
+    int ans = 0;
+    for(int i = 0; i < numsSize - 2; i++){
+        for(int j = i + 1; j < numsSize - 1; j++){
+            if(nums[j] - nums[i] > diff){
+                break;
+            }else if(nums[j] - nums[i] == diff){
+                for(int k = j + 1; k < numsSize; k++){
+                    if(nums[k] - nums[j] > diff){
+                        break;
+                    }else if(nums[k] - nums[j] == diff){
+                        ans++;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return ans;
+}
