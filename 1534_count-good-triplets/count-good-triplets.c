bc1fc991987b65d248d13dff751f6439f8295088
PinJimWu <pinjim2006@gmail.com>
2026-02-28 18:35:21 +0800

    Add/Update 1534_count-good-triplets (c)

--- 1534_count-good-triplets/count-good-triplets.c
+++ 1534_count-good-triplets/count-good-triplets.c
@@ -2,8 +2,9 @@ int countGoodTriplets(int* arr, int arrSize, int a, int b, int c){
     int ans = 0;
     for(int i = 0; i < arrSize - 2; i++){
         for(int j  = i + 1; j < arrSize - 1; j++){
+            if(abs(arr[i] - arr[j]) > a) continue;
             for(int k = j + 1; k < arrSize; k++){
-                if(abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[k] - arr[i]) <= c) ans++;
+                if(abs(arr[j] - arr[k]) <= b && abs(arr[k] - arr[i]) <= c) ans++;
             }
         }
     }
