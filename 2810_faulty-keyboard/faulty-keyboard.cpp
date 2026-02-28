9d2b352c39029944fbf7386083678cfd54dfc6e6
PinJimWu <pinjim2006@gmail.com>
2026-02-28 18:38:02 +0800

    Add/Update 2810_faulty-keyboard (cpp)

---
+++ 2810_faulty-keyboard/faulty-keyboard.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string result = "";
+        for(int i = 0; s[i] != '\0'; i++){
+            if(s[i] == 'i'){
+                reverse(result.begin(), result.end());
+            }else{
+                result += s[i];
+            }
+        }
+        return result;
+    }
+};
