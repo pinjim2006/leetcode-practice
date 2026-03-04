0e2a66641342d59f3ea2e0ad6610297269d0ad1d
PinJimWu <pinjim2006@gmail.com>
2026-03-04 16:42:24 +0800

    Add/Update 1582_special-positions-in-a-binary-matrix (c)

--- 1582_special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.c
+++ 1582_special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.c
@@ -1,18 +1,16 @@
 int numSpecial(int** mat, int matSize, int* matColSize) {
-    int col[matColSize[0]],row[matSize],ans=0;
-    memset(col,0,sizeof(col));
-    memset(row,0,sizeof(row));
-    for(int i=0;i<matSize;i++){
-        for(int j=0;j<matColSize[0];j++){
+    int col[matColSize[0]] = {}, row[matSize] = {}, ans = 0;
+    for(int i = 0; i < matSize; i++){
+        for(int j = 0; j < matColSize[0]; j++){
             if(mat[i][j]){
                 col[j]++;
                 row[i]++;
             }
         }
     }
-    for(int i=0;i<matSize;i++){
-        for(int j=0;j<matColSize[0];j++){
-            if(col[j]==1&&row[i]==1&&mat[i][j]==1){
+    for(int i = 0; i < matSize; i++){
+        for(int j = 0; j < matColSize[0]; j++){
+            if(col[j] == 1 && row[i] == 1 && mat[i][j]==1){
                 ans++;
             }
         }
