23f8507d27f6dfb8c28eb12d98f473c5cfb0e204
PinJimWu <pinjim2006@gmail.com>
2026-03-18 01:55:00 +0800

    Add/Update 189_rotate-array (c)

---
+++ 0189_rotate-array/rotate-array.c
@@ -0,0 +1,25 @@
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+void rotate(int* nums, int numsSize, int k) {
+    k %= numsSize;
+    if(k == 0) return;
+    int n = gcd(k, numsSize);
+    for(int i = 0; i < n; i++){
+        int last = nums[i];
+        int loc = i;
+        for(int i = 0; i < numsSize / n; i++){
+            loc += k;
+            loc %= numsSize;
+            int tmp = nums[loc];
+            nums[loc] = last;
+            last = tmp;
+        }
+    }
+}
