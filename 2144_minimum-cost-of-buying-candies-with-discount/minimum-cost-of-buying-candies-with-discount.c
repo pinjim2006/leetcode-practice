d4d823ed539656f871245c064cb484c557b5d295
pinjim2006 <pinjim2006@gmail.com>
2026-06-02 01:49:49 +0800

    Add/Update 2144_minimum-cost-of-buying-candies-with-discount (c)

---
+++ 2144_minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.c
@@ -0,0 +1,13 @@
+int cmp(const void* a, const void* b){
+    return *(int*)b - *(int*)a;
+}
+
+int minimumCost(int* cost, int costSize) {
+    qsort(cost, costSize, sizeof(int), cmp);
+    int ans = 0;
+    for(int i = 0; i < costSize; i++){
+        if(i % 3 == 2) continue;
+        ans += cost[i];
+    }
+    return ans;
+}
