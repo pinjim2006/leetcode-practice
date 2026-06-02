f70efb0dd475c1a6ec9a34d8f355a36255eb437e
pinjim2006 <pinjim2006@gmail.com>
2026-06-03 02:39:24 +0800

    Add/Update 3633_earliest-finish-time-for-land-and-water-rides-i (c)

---
+++ 3633_earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.c
@@ -0,0 +1,19 @@
+int earliestFinishTime(int* landStartTime, int landStartTimeSize, int* landDuration, int landDurationSize, int* waterStartTime, int waterStartTimeSize, int* waterDuration, int waterDurationSize) {
+    int minLandEnd = 10000, minWaterEnd = 10000;
+    int minTime = 10000;
+    for(int i = 0; i < landStartTimeSize; i++){
+        minLandEnd = (landStartTime[i] + landDuration[i]) < minLandEnd ? (landStartTime[i] + landDuration[i]) : minLandEnd;
+    }
+    for(int i = 0; i < waterStartTimeSize; i++){
+        minWaterEnd = (waterStartTime[i] + waterDuration[i]) < minWaterEnd ? (waterStartTime[i] + waterDuration[i]) : minWaterEnd;
+    }
+    for(int i = 0; i < waterStartTimeSize; i++){
+        int tmp = (minLandEnd + waterDuration[i]) > (waterStartTime[i] + waterDuration[i]) ? (minLandEnd + waterDuration[i]) : (waterStartTime[i] + waterDuration[i]);
+        minTime = tmp < minTime ? tmp : minTime;
+    }
+    for(int i = 0; i < landStartTimeSize; i++){
+        int tmp = (minWaterEnd + landDuration[i]) > (landStartTime[i] + landDuration[i]) ? (minWaterEnd + landDuration[i]) : (landStartTime[i] + landDuration[i]);
+        minTime = tmp < minTime ? tmp : minTime;
+    }
+    return minTime;
+}
